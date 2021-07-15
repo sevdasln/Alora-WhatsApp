f482c488986c4e9ddad5f315d2d2e8f51a6e1db6
gizako <86647496+gizako@users.noreply.github.com>
2021-07-15 20:07:06 +0530

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -63,7 +63,7 @@ if (Config.WORKTYPE == 'private') {
                 }
             );
             await message.client.sendMessage(
-                message.jid,'●▬▬▬ *WhatsAsena Private* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
+                message.jid,'●▬▬▬ 🇬 🇮 🇸 🇭 🇦 🇳   🇧 🇴 🇹 ▬▬▬●\n\n' + CMD_HELP, MessageType.text
             );    
         } else {
             var CMD_HELP = '';
@@ -116,7 +116,7 @@ if (Config.WORKTYPE == 'private') {
             );
             if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
             await message.client.sendMessage(
-                message.jid,'●▬▬▬ *WhatsAsena Private* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
+                message.jid,'●▬▬▬ 🇬 🇮 🇸 🇭 🇦 🇳   🇧 🇴 🇹 ▬▬▬●\n\n' + CMD_HELP, MessageType.text
             );
         }
     }));
@@ -171,7 +171,7 @@ else if (Config.WORKTYPE == 'public') {
                 }
             );
             await message.client.sendMessage(
-                message.jid,'●▬▬▬ *WhatsAsena Public* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
+                message.jid,'●▬▬▬ 🇬 🇮 🇸 🇭 🇦 🇳   🇧 🇴 🇹 ▬▬▬●\n\n' + CMD_HELP, MessageType.text
             );    
         } else {
             var CMD_HELP = '';
@@ -224,7 +224,7 @@ else if (Config.WORKTYPE == 'public') {
             );
             if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
             await message.client.sendMessage(
-                message.jid,'●▬▬▬ *WhatsAsena Public* ▬▬▬●\n\n' + CMD_HELP, MessageType.text
+                message.jid,'●▬▬▬ 🇬 🇮 🇸 🇭 🇦 🇳   🇧 🇴 🇹 ▬▬▬●\n\n' + CMD_HELP, MessageType.text
             );
         }
     }));
