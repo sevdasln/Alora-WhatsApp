f534f82cdbd41a795952eb68c5e1538c28f70fc7
xRASHMITH <95857023+xRASHMITH@users.noreply.github.com>
2021-12-12 18:08:21 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -153,126 +153,126 @@ ${chalk.blue.italic('👿 Connecting to WhatsApp...▶')}`);
                 var ov_time = new Date().toLocaleString('LK', { timeZone: 'Europe/Istanbul' }).split(' ')[1]
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('994')) { 
                 var ov_time = new Date().toLocaleString('AZ', { timeZone: 'Asia/Baku' }).split(' ')[1]
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('94')) { 
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
                 var ov_time = new Date().toLocaleString('LK', { timeZone: 'Asia/Colombo' }).split(' ')[1]
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('351')) { 
                 var ov_time = new Date().toLocaleString('PT', { timeZone: 'Europe/Lisbon' }).split(' ')[1]
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('75')) { 
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
                 var ov_time = new Date().toLocaleString('RU', { timeZone: 'Europe/Kaliningrad' }).split(' ')[1]
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('91')) { 
                 var ov_time = new Date().toLocaleString('HI', { timeZone: 'Asia/Kolkata' }).split(' ')[1]
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('62')) { 
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
                 var ov_time = new Date().toLocaleString('ID', { timeZone: 'Asia/Jakarta' }).split(' ')[1]
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('49')) { 
                 var ov_time = new Date().toLocaleString('DE', { timeZone: 'Europe/Berlin' }).split(' ')[1]
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('61')) {  
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
                 var ov_time = new Date().toLocaleString('AU', { timeZone: 'Australia/Lord_Howe' }).split(' ')[1]
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('55')) { 
                 var ov_time = new Date().toLocaleString('BR', { timeZone: 'America/Noronha' }).split(' ')[1]
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('33')) {
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
                 var ov_time = new Date().toLocaleString('FR', { timeZone: 'Europe/Paris' }).split(' ')[1]
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('34')) { 
                 var ov_time = new Date().toLocaleString('ES', { timeZone: 'Europe/Madrid' }).split(' ')[1]
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('44')) { 
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
                 var ov_time = new Date().toLocaleString('GB', { timeZone: 'Europe/London' }).split(' ')[1]
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('39')) {  
                 var ov_time = new Date().toLocaleString('IT', { timeZone: 'Europe/Rome' }).split(' ')[1]
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('7')) { 
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
                 var ov_time = new Date().toLocaleString('KZ', { timeZone: 'Asia/Almaty' }).split(' ')[1]
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('998')) {  
                 var ov_time = new Date().toLocaleString('UZ', { timeZone: 'Asia/Samarkand' }).split(' ')[1]
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time + '\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else if (conn.user.jid.startsWith('993')) { 
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
                 var ov_time = new Date().toLocaleString('TM', { timeZone: 'Asia/Ashgabat' }).split(' ')[1]
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
             else {
                 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
                 var utch = new Date().toLocaleDateString(config.LANG, get_localized_date)
                 var ov_time = new Date().toLocaleString('EN', { timeZone: 'America/New_York' }).split(' ')[1]
-                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👿 T-REX BOT 👿'
+                const biography = '📅 ' + utch + '\n⌚ ' + ov_time +'\n\nPOWERD BY 👸 ALORA BOT ✅'
                 await conn.setStatus(biography)
             }
         }
@@ -284,9 +284,9 @@ ${chalk.blue.italic('👿 Connecting to WhatsApp...▶')}`);
          
         while (getGMTh == 19 && getGMTm == 1) {
             var announce = ''
-            if (config.LANG == 'EN') announce = '📢◉◉ \n👾Announcement SYSTEM 🔘'
-            if (config.LANG == 'SI') announce = '📢◉◉ \n👾නිවේදන පද්ධතිය 🔘'
-            if (config.LANG == 'ID') announce = '📢◉◉ \n👾Announcement System 🔘'
+            if (config.LANG == 'EN') announce = '📢☉☉☈ \n✅➣Announcement SYSTEM 🔰'
+            if (config.LANG == 'SI') announce = '📢☉☉☈ \n✅➣නිවේදන පද්ධතිය 🔰'
+            if (config.LANG == 'ID') announce = '📢☉☉☈ \n✅➣Announcement System 🔰'
             
             let video = 'https://m.youtube.com/watch?v=dNY-dXYc8-I'
             let image = 'https://i.ibb.co/xY47y3L/20211210-075024.jpg'
