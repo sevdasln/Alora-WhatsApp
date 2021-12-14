1d1cdc49810bd398fcf65ef8d92f295542567d91
xRASHMITH <95857023+xRASHMITH@users.noreply.github.com>
2021-12-14 20:10:27 +0530

    Create NEWTEST.js

---
+++ plugins/NEWTEST.js
@@ -0,0 +1,15 @@
+ /*╭─「 ᴄᴏᴍᴍᴀɴᴅꜱ ᴘᴀɴᴇʟ」*
+ │
+ │ᴛʏᴘᴇ: .`+Config.PANEL_COMMAND+` get full Command list*
+ ├─────────●●►
+ │🌼HEY,*  `+wish+`
+ │👤Name:* `+Config.BOT_NAME+`
+ │💎Role:* Developer
+ │Time⌚:* `+time+`
+ │📡Server:* Digital Ocean
+ │📦Frameworks:* Nodejs
+ │☬Working As*
+ │🛡️ `+Config.WORKTYPE+`*
+ │ 
+ ╰──────────●●►
+*/
