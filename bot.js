acc92a311f1d36ff8705f9f4af177d25ed63932b
xRASHMITH <95857023+xRASHMITH@users.noreply.github.com>
2021-12-19 07:05:42 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,9 @@
-/* Copyright (C) 2021 ALORA
+/*
+█▀▀█ █░░ █▀▀█ █▀▀█ █▀▀█   █▀▀▄ █▀▀█ ▀▀█▀▀
+█▄▄█ █░░ █░░█ █▄▄▀ █▄▄█   █▀▀▄ █░░█ ░░█░░
+▀░░▀ ▀▀▀ ▀▀▀▀ ▀░▀▀ ▀░░▀   ▀▀▀░ ▀▀▀▀ ░░▀░░
+
+Copyright (C) 2021 ALORA
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 ALORA ➣ YAZUWA
