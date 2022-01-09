0160798b00bdc0d32d52f15b5b1e9e3b46f18c3a
xRASHMITH <95857023+xRASHMITH@users.noreply.github.com>
2022-01-09 09:13:00 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -1,82 +1,75 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Credit goes to BlackAmda*/
 
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
-Developer & Co-Founder - Phaticusthiccy
-*/
 
-const Asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
+const Asena = require('../events')
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const {spawnSync} = require('child_process');
-const Config = require('../config');
 const chalk = require('chalk');
-const Axios = require('axios');
+const axios = require('axios');
+const fs = require('fs');
 
 const Language = require('../language');
 const Lang = Language.getString('system_stats');
 
+ if (Config.WORKTYPE == 'public')
 
-if (Config.WORKTYPE == 'private') {
+var SYSDTXT = ''
+if (Build.LANG == 'SI') SYSDTXT = '💻 පද්ධති තත්ත්වය'
+if (Build.LANG == 'EN') SYSDTXT = '💻 System status'
 
-    Asena.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+var VER = ''
+if (Build.LANG == 'SI') VER = '🧬 Version'
+if (Build.LANG == 'EN') VER = '🧬 Version'
 
-        if (Config.ALIVEMSG == 'default') {
-            await message.client.sendMessage(message.jid,'╭──────────◅\n│\n│🎧ʜᴇʟʟᴏ ᴜꜱᴇʀ\n│╭──────────────╮\n│👸Ｉ ＡＭ A L I V E  \n👸  │╰──────────────╯\n│\n├►ɪ ᴀᴍ ᴀʟᴏʀᴀ ʙᴏᴛ\n│\n├▻ᴠᴇʀꜱɪᴏɴ - ᴡɪᴛʜᴏᴜᴛ ʙᴜᴛᴛᴏɴꜱ\n│\n├▻ᴅᴇᴠᴇʟᴏᴘᴇʀ - xY̷A̷Z̷U̷W̷A̷\n│\n├▻ᴍᴇɴᴜ ᴄᴏᴍᴍᴀɴᴅ - .alora\n│\n│💞ᴛʜᴀɴᴋ ʏᴏᴜ ꜰᴏʀ ᴜꜱɪɴɢ ᴍᴇ👸\n│\n╰────────────▻\n\nـــ٨ـہہـ♡ـ٨ـہـ' , MessageType.text);
-        }
-        else {
-            const pow = '*👸Powered by Alora✅*'
-            const payload = Config.ALIVEMSG
-            const status = await message.client.getStatus()
-            const ppUrl = await message.client.getProfilePicture() 
-            const resim = await Axios.get(ppUrl, {responseType: 'arraybuffer'})
+var MSG = ''
+if (Build.ALIVEMSG == 'default') MSG = '```Hey There! Bot Online now. 💃🏻♥️```\n\n*Developer:* ```Black Amda```\n\n*Official Website:* https://amdaniwasa.com\n\n*Youtube channel :* https://www.youtube.com/channel/UCZx8U1EU95-Wn9mH4dn15vQ\n\n```Thank You For Using Queen Amdi💞```'
+else MSG = Build.ALIVEMSG
 
-            if (!payload.includes('{pp}')) {
-                await message.client.sendMessage(message.jid,payload.replace('{version}', Config.VERSION).replace('{info}', `${status.status}`).replace('{plugin}', Config.CHANNEL) + '\n' + pow, MessageType.text);
-            }
-            else if (payload.includes('{pp}')) {
-                await message.sendMessage(Buffer(resim.data), MessageType.image, { caption: payload.replace('{version}', Config.VERSION).replace('{pp}', '').replace('{info}', `${status.status}`).replace('{plugin}', Config.CHANNEL) + '\n' + pow });
-            }
-        }
-    }));
 
-    Asena.addCommand({pattern: 'sysd', fromMe: true, desc: Lang.SYSD_DESC}, (async (message, match) => {
+Asena.addCommand({pattern: 'alive', fromMe: Work_Mode, desc: Lang.ALIVE_DESC,  deleteCommand: false}, (async (message, match) => {
+    await QueenAmdi.amdi_setup()
+    var logo = await axios.get (Build.ALIVE_LOGO, {responseType: 'arraybuffer'})
+    var PIC = Buffer.from(logo.data)
 
-        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
-        await message.sendMessage(
-            '```' + child + '```', MessageType.text
-        );
-    }));
-}
-else if (Config.WORKTYPE == 'public') {
+    const media = await message.client.prepareMessage(message.jid, PIC, MessageType.image, { thumbnail: PIC })
 
-    Asena.addCommand({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+    var BUTTHANDLE = '';
+    if (/\[(\W*)\]/.test(Build.HANDLERS)) {
+        BUTTHANDLE = Build.HANDLERS.match(/\[(\W*)\]/)[1][0];
+    } else {
+        BUTTHANDLE = '.';
+    }
+        
+    const buttons = [
+        {buttonId: BUTTHANDLE + 'qaversion', buttonText: {displayText: VER }, type: 1},
+        {buttonId: BUTTHANDLE + 'qasysstats', buttonText: {displayText: SYSDTXT }, type: 1}
+    ]
+    const buttonMessage = {
+        contentText: MSG,
+        footerText: 'ǫᴜᴇᴇɴ ᴀʟᴏʀᴀ © ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ',
+        buttons: buttons,
+        headerType: 4,
+        imageMessage: media.message.imageMessage    
+    }
+    await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage);
+}))
 
-        if (Config.ALIVEMSG == 'default') {
-            await message.client.sendMessage(message.jid,'╭──────────◅\n│\n│🎧ʜᴇʟʟᴏ ᴜꜱᴇʀ\n│╭──────────────╮\n│👸Ｉ ＡＭ A L I V E  \n👸  │╰──────────────╯\n│\n├►ɪ ᴀᴍ ᴀʟᴏʀᴀ ʙᴏᴛ\n│\n├▻ᴠᴇʀꜱɪᴏɴ - ᴡɪᴛʜᴏᴜᴛ ʙᴜᴛᴛᴏɴꜱ\n│\n├▻ᴅᴇᴠᴇʟᴏᴘᴇʀ - xY̷A̷Z̷U̷W̷A̷\n│\n├▻ᴍᴇɴᴜ ᴄᴏᴍᴍᴀɴᴅ - .alora\n│\n│💞ᴛʜᴀɴᴋ ʏᴏᴜ ꜰᴏʀ ᴜꜱɪɴɢ ᴍᴇ👸\n│\n╰────────────▻\n\nـــ٨ـہہـ♡ـ٨ـہـ' , MessageType.text);
-        }
-        else {
-            const pow = '*❯👸ρσɯҽɾҽԃ Ⴆყ αʅσɾα Ⴆσƚ✮❮*'
-            const payload = Config.ALIVEMSG
-            const status = await message.client.getStatus()
-            const ppUrl = await message.client.getProfilePicture() 
-            const resim = await Axios.get(ppUrl, {responseType: 'arraybuffer'})
+Asena.addCommand({pattern: 'qasysstats', fromMe: Work_Mode, desc: Lang.SYSD_DESC, dontAddCommandList: true,  deleteCommand: false}, (async (message, match) => {
+    await QueenAmdi.amdi_setup()
+    const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
+    await message.sendMessage(
+        '```' + child + '```', MessageType.text, {quoted: message.data}
+    );
+}));
 
-            if (!payload.includes('{pp}')) {
-                await message.client.sendMessage(message.jid,payload.replace('{version}', Config.VERSION).replace('{info}', `${status.status}`).replace('{plugin}', Config.CHANNEL) + '\n' + pow, MessageType.text);
-            }
-            else if (payload.includes('{pp}')) {
-                await message.sendMessage(Buffer(resim.data), MessageType.image, { caption: payload.replace('{version}', Config.VERSION).replace('{pp}', '').replace('{info}', `${status.status}`).replace('{plugin}', Config.CHANNEL) + '\n' + pow });
-            }
-        }
-    }));
-
-    Asena.addCommand({pattern: 'sysd', fromMe: false, desc: Lang.SYSD_DESC}, (async (message, match) => {
-
-        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
-        await message.sendMessage(
-            '```' + child + '```', MessageType.text
-        );
-    }));
-}
+Asena.addCommand({pattern: 'qaversion', fromMe: Work_Mode, desc: Lang.BOT_V, dontAddCommandList: true,  deleteCommand: false}, (async (message, match) => {
+    await QueenAmdi.amdi_setup()
+    await message.client.sendMessage(message.jid, 
+        `*✨ Alora Version 🎉*\n\n` + 
+        '```Installed version :```\n' +
+        Lang.version + 
+        `\n\nCheck official website : https://....../`
+   , MessageType.text, {quoted: message.data});
+    
+}));
