593378011729494dd726f4a82f165878d38d8e71
xRASHMITH <95857023+xRASHMITH@users.noreply.github.com>
2022-01-11 20:26:22 +0530

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -1,5 +1,12 @@
 /* Credit goes to BlackAmda*/
 
+/* Copyright (C) 2021 xYAZUWA
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Alora 🛠️ xRASHMITH
+*/
 
 
 
@@ -13,9 +20,15 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('system_stats');
 
- let tk = Config.WORKTYPE == 'public' ? false: true
+//𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝𝕝
 
+if (Config.WORKTYPE == 'public') {
 
+    Asena.addCommand({pattern: 'alive', fromMe: false, desc: Lang.ALIVE_DESC}, (async (message, match) => {
+
+        if (Config.ALIVEMSG == 'default') {
+            
+    
 var SYSDTXT = ''
 if (Config.LANG == 'SI') SYSDTXT = '📣 පද්ධති තත්ත්වය'
 if (Config.LANG == 'EN') SYSDTXT = '📣 System stats'
@@ -25,52 +38,48 @@ if (Config.LANG == 'SI') VER = '📲 Version'
 if (Config.LANG == 'EN') VER = '📲 Version'
 
 var MSG = ''
-if (Config.ALIVEMSG == 'default') MSG = '╭──────────◅\n│\n│🎧ʜᴇʟʟᴏ ᴜꜱᴇʀ\n│╭──────────────╮\n│👸Ｉ ＡＭ A L I V E  \n👸  │╰──────────────╯\n│\n├►ɪ ᴀᴍ ᴀʟᴏʀᴀ ʙᴏᴛ\n│\n├▻ᴠᴇʀꜱɪᴏɴ - ᴡɪᴛʜᴏᴜᴛ ʙᴜᴛᴛᴏɴꜱ\n│\n├▻ᴅᴇᴠᴇʟᴏᴘᴇʀ - xY̷A̷Z̷U̷W̷A̷\n│\n├▻ᴍᴇɴᴜ ᴄᴏᴍᴍᴀɴᴅ - .alora\n│\n│💞ᴛʜᴀɴᴋ ʏᴏᴜ ꜰᴏʀ ᴜꜱɪɴɢ ᴍᴇ👸\n│\n╰────────────▻\n\nـــ٨ـہہـ♡ـ٨ـہـ'
+if (Config.ALIVEMSG == 'default') MSG = '╭──────────◅\n│\n│🎧ʜᴇʟʟᴏ ᴜꜱᴇʀ\n│╭──────────────╮\n│👸Ｉ ＡＭ A L I V E  \n👸  │╰──────────────╯\n│\n├►ɪ ᴀᴍ ᴀʟᴏʀᴀ ʙᴏᴛ\n│\n├▻ᴠᴇʀꜱɪᴏɴ - ᴡɪᴛʜ ʙᴜᴛᴛᴏɴꜱ\n│\n├▻ᴅᴇᴠᴇʟᴏᴘᴇʀ - xY̷A̷Z̷U̷W̷A̷\n│\n├▻ᴍᴇɴᴜ ᴄᴏᴍᴍᴀɴᴅ - .alora\n│\n│💞ᴛʜᴀɴᴋ ʏᴏᴜ ꜰᴏʀ ᴜꜱɪɴɢ ᴍᴇ👸\n│\n╰────────────▻\n\nـــ٨ـہہـ♡ـ٨ـہـ'
 else MSG = Config.ALIVEMSG
-
-
-Asena.addCommand({pattern: 'alive', fromMe: tk, desc: Lang.ALIVE_DESC,  deleteCommand: false}, (async (message, match) => {
-    await QueenAmdi.amdi_setup()
-    var logo = await axios.get (Config.ALIVE_LOGO, {responseType: 'arraybuffer'})
-    var PIC = Buffer.from(logo.data)
-
-
-    var BUTTHANDLE = '';
-    if (/\[(\W*)\]/.test(Config.HANDLERS)) {
-        BUTTHANDLE = HANDLERS.match(/\[(\W*)\]/)[1][0];
-    } else {
-        BUTTHANDLE = '.';
-    }
         
-    const buttons = [
-        {buttonId: BUTTHANDLE + 'qaversion', buttonText: {displayText: VER }, type: 1},
-        {buttonId: BUTTHANDLE + 'qasysstats', buttonText: {displayText: SYSDTXT }, type: 1}
-    ]
-    const buttonMessage = {
-        contentText: MSG,
-        footerText: 'ǫᴜᴇᴇɴ ᴀʟᴏʀᴀ © ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ',
-        buttons: buttons,
-        headerType: 4,
-        imageMessage: 'https://i.ibb.co/xY47y3L/20211210-075024.jpg:   
-    }
-    await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage);
-}))
 
-Asena.addCommand({pattern: 'qasysstats', fromMe: tk, desc: Lang.SYSD_DESC, dontAddCommandList: true,  deleteCommand: false}, (async (message, match) => {
-    await QueenAmdi.amdi_setup()
-    const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
-    await message.sendMessage(
-        '```' + child + '```', MessageType.text, {quoted: message.data}
-    );
-}));
+const buttons = [
+  {buttonId: BUTTHANDLE + 'qaversion', buttonText: {displayText: SYSDTXT }, type: 1},
+  {buttonId: BUTTHANDLE + 'qasysstats', buttonText: {displayText: VER }, type: 1},
+ ]
 
-Asena.addCommand({pattern: 'qaversion', fromMe: tk, desc: Lang., dontAddCommandList: true,  deleteCommand: false}, (async (message, match) => {
-    await QueenAmdi.amdi_setup()
-    await message.client.sendMessage(message.jid, 
-        `*✨ Alora Version 🎉*\n\n` + 
-        '```Installed version :```\n' +
-        Lang.version + 
-        `\n\nCheck official website : https://....../`
-   , MessageType.text, {quoted: message.data});
+const btn = {
+    imageMessage: 'https://i.ibb.co/xY47y3L/20211210-075024.jpg',
+    contentText: MSG ,
+    footerText: 'ᴀʟᴏʀᴀ ᴘᴜʙʟɪᴄ sᴛᴀʙʟᴇシ︎',
+    buttons: buttons,
+    headerType: 4
+}
+
+  await message.client.sendMessage (message.jid, btn, MessageType.buttonsMessage)
+}
+    }));
+
+    Asena.addCommand({pattern: 'qasysstats', fromMe: false,  deleteCommand: false,  desc: Lang.SYSD_DESC, dontAddCommandList: true}, (async (message, match) => {
+
+        if (message.jid === '393475528094-1415817281@g.us') {
+
+            return;
+        }
+
+        const child = spawnSync('neofetch', ['--stdout']).stdout.toString('utf-8')
+        await message.sendMessage(
+            '```' + child + '```', MessageType.text
+        );
+    }));
+
+    Asena.addCommand({pattern: 'qaversion', fromMe: false, desc: Lang.BOT_V}, (async (message, match) => {    
     
-}));
+        await message.client.sendMessage(message.jid, 
+                `*🌹𝖰𝗎𝖾𝖾𝗇 𝖠𝗅𝗈𝗋𝖺 *\n\n` + 
+                '```Installed version :```\n' +
+                ' V 2.5.0 - Public'+
+                `\n\n🧰 Check github for bot:  https://github.com/xYAZUWA/AloraButtons`
+           , MessageType.text);
+            
+        }));
+}
