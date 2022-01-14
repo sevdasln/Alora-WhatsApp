afab0cc54282c428f89e33c1ab5cd12c191cf9dc
Yasindu Rashmith <95857023+xRASHMITH@users.noreply.github.com>
2022-01-14 18:11:34 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,7 +72,7 @@ async function whatsAsena () {
     
 // ════════════════════WA CONNECTION🔰🔰🔰
     const conn = new WAConnection();
-    conn.version = [3, 3234, 9];
+    conn.version = [2, 2140, 12];
     const Session = new StringSession();
     conn.browserDescription = ['Alora', 'Edge', '96'];
     
