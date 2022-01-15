83c2018d6bde04c2bc1fe330a3f03abc903fbeec
Yasindu Rashmith <95857023+xRASHMITH@users.noreply.github.com>
2022-01-15 21:14:56 +0530

    Create NormalMenu.js

---
+++ plugins/NormalMenu.js
@@ -0,0 +1,17 @@
+const Asena = require('../events');
+const {
+    MessageType,
+    GroupSettingChange,
+    Mimetype,
+    MessageOptions
+} = require('@adiwajshing/baileys');
+const fs = require('fs');
+const Config = require('../config')
+const axios = require('axios')
+const request = require('request');
+const os = require('os');
+
+let tk = Config.WORKTYPE == 'public' ? false: true
+
+Asena.addCommand({ pattern: 'alora', fromMe: tk, desc: 'down;load passpepr'
+}, (async (message, match) => {
