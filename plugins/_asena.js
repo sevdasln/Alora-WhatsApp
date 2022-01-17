af97edd6e49f7c9f92d413f5ffea9ee228eadcdd
Yasindu Rashmith <95857023+xRASHMITH@users.noreply.github.com>
2022-01-17 13:33:24 +0530

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -8,7 +8,7 @@ WhatsAsena - Yusuf Usta
 
 const Asena = require('../events');
 const Config = require('../config');
-const {MessageType,Mimetype,MessageOptions} = require('@adiwajshing/baileys');
+const {WAConnection, MessageOptions, MessageType, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 
 const Language = require('../language');
