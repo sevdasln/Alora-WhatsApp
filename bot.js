fa63d43d0b7e07f9f6ae120ae4dfe1e38dab5959
Yasindu Rashmith <95857023+xRASHMITH@users.noreply.github.com>
2022-01-26 12:29:21 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -303,7 +303,7 @@ async function whatsAsena () {
         );
         const authInfo = conn.base64EncodedAuthInfo();
         if (StrSes_Db.length < 1) {
-            await WhatsAsenaDB.create({ info: "StringSession", value: Session.createStringSession(authInfo) });
+            await AloraDB.create({ info: "StringSession", value: Session.createStringSession(authInfo) });
         } else {
             await StrSes_Db[0].update({ value: Session.createStringSession(authInfo) });
         }
